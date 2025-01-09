2ee66af5de502164dc275e934e8daa0b2a8e5c5d
Yehoshua Cohen <100349784+Shuicohen@users.noreply.github.com>
2025-01-09 11:01:02 +0200

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -7,7 +7,7 @@ import axios from "axios";
  */
 export const fetchIPDetails = async (ipAddress) => {
   try {
-    const response = await axios.get(`http://localhost:5000/api/ip/${ipAddress}`);
+    const response = await axios.get(`https://ip-address-tracker-410z.onrender.com/api/ip/${ipAddress}`);
     return response.data;
   } catch (error) {
     console.error("Error fetching IP details:", error);
